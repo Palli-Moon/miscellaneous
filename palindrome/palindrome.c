cb9adfaefc3732a2a630b35e0626e36f7acb6572
Palli-Moon <pallimoon@gmail.com>
2016-11-01 16:34:46 +0000

    Add basic skeleton for palindrome script

---
+++ palindrome/palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int palindrome(char *s);
+
+int main() {
+    palindrome("palindrome");
+
+    return 0;
+}
+
+int palindrome(char *s) 
+{
+    printf("%s\n", s);
+
+    return 0;
+}
